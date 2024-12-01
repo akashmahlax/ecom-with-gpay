7218257e0bf24ede8b9a9639e09bfb1679ad8839
akashmahlax <akashdalla407@gmail.com>
2024-12-01 18:15:18 +0530

    add to cart button added

--- ecommerce-web/src/components/Navbar.js
+++ ecommerce-web/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
-      <h1 className="text-2xl font-bold">ShopEase</h1>
+      <h1 className="text-2xl font-bold">Akash Shop</h1>
       <div className="flex space-x-6">
         <NavLink to="/" className="hover:text-yellow-500">Home</NavLink>
         <NavLink to="/products" className="hover:text-yellow-500">Products</NavLink>
