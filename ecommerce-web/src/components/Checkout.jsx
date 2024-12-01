053dc7b35fb765c4878683e957c58b23a3d82492
akashmahlax <akashdalla407@gmail.com>
2024-12-01 11:35:54 +0530

    checkout update

--- ecommerce-web/src/components/Checkout.jsx
+++ ecommerce-web/src/components/Checkout.jsx
@@ -73,8 +73,13 @@ const App = () => {
     initializeGooglePay();
   }, []);
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-400">
-      <img src="https://images.unsplash.com/photo-1506193095-80bc749473f2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2F0Y2h8ZW58MHx8MHx8fDA%3D" alt="product img" className="w-40 h-40 mb-6 rounded-2xl border border-2 border-violet-600" />
+    <div className="min-h-screen flex flex-col items-center justify-center border border-2 border-violet-600 bg-gray-400">
+      <div  className="flex gap-2 items-center justify-center border border-2 border-slate-400 rounded-2xl bg-gray-300" >
+      <img src="https://images.unsplash.com/photo-1506193095-80bc749473f2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2F0Y2h8ZW58MHx8MHx8fDA%3D" alt="product img" className="w-40 h-40 mb-6 m-2 rounded-2xl " />
+      <div className="w-40 h-40 mb-6">
+        <h1>about</h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor illo pariatur veritatis saepe expedita cum quasi quos illum similique quia? </div>
+      </div>
       <h1 className="text-3xl font-bold mb-6">Buy this product</h1>
       <p className="text-lg text-black mb-4">Price: ₹1</p>
       <div id="google-pay-button"></div>
